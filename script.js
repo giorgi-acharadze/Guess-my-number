af813084bc3b92ead80805cf3371674df190cea4
George <acharadzeg@gmail.com>
2023-03-25 18:45:46 +0400

    some js edits

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = number(document.querySelector('.guess').value);
+  console.log(guess);
+});
